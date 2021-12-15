7545322bbc44247a1e3daaf2d345a852831158e8
Maria Saavedra <msaav3@gmail.com>
2021-12-15 00:58:40 -0600

    WIP

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,47 +1,50 @@
 // GLOBAL VARS & TYPES
-let numberOfShapesControl: p5.Element;
-
-// P5 WILL AUTOMATICALLY USE GLOBAL MODE IF A DRAW() FUNCTION IS DEFINED
+//let numberOfShapesControl: p5.Element;
+const iterationCount = 100;
+let yoff = 0.0; 
+let xoff = -12;
+// P5 will automatically use global mode if a draw() function is defined
 function setup() {
   console.log("🚀 - Setup initialized - P5 is running");
-
+  stroke(255); // Set line drawing color to white
+  frameRate(30);
   createCanvas(windowWidth, windowHeight)
-  rectMode(CENTER).noFill().frameRate(30);
-  // NUMBER OF SHAPES SLIDER
-  numberOfShapesControl = createSlider(1, 30, 15, 1).position(10, 10).style("width", "100px");
 }
-
-// p5 WILL AUTO RUN THIS FUNCTION IF THE BROWSER WINDOW SIZE CHANGES
+// called on window resize.
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-// p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
+function makeWave(){
+    // We are going to draw a polygon out of the wave points
+    beginShape(); 
+       // Option #1: 2D Noise
+    // float xoff = yoff; // Option #2: 1D Noise
+    // Iterate over horizontal pixels
+    for (let x = -1200; x <= width; x += 5) {
+      // Calculate a y value according to noise, map to 
+      const y = map(noise(xoff, yoff), 0, .75, 300, 1); // Option #1: 2D Noise
+      // float y = map(noise(xoff), 0, 1, 200,300);    // Option #2: 1D Noise
+      // Set the vertex
+      vertex(x, y); 
+      // Increment x dimension for noise
+      xoff += 0.05;
+    }
+    // increment y dimension for noise
+    yoff += 0.01;
+    vertex(width, height);
+    vertex(0, height);
+    endShape(CLOSE);
+}
+// p5 Will handle requesting animation frames from the browser and wil run draw() each animation frrame.
 function draw() {
-  
-   // CLEAR BACKGROUND
   background(0);
-
-  // CENTER OF SCREEN
-  translate(width / 2,height / 2);
-
-  const numberOfShapes = <number>numberOfShapesControl.value();
-  const colours = ColorHelper.getColorsArray(numberOfShapes);
-
-  // CONSISTENT SPEED REGARDLESS OF FRAMERATE
-  const speed = (frameCount / (numberOfShapes * 30)) * 2;
-  
-  // DRAW ALL SHAPES
-  for (var i = 0; i < numberOfShapes; i++) {
-    push();
-      const lineWidth = 8;
-      const spin = speed * (numberOfShapes - i);
-      const numberOfSides = 3 + i;
-      const width = 40 * i;
-      strokeWeight(lineWidth); 
-      stroke(colours[i]);
-      rotate(spin);
-      PolygonHelper.draw(numberOfSides, width)
-    pop();
-  }
+  translate(width / 2,height / 2);   // center of screen
+  fill(255);
+  circle(100, -300, 20);
+  makeWave();
+  makeWave();
+  makeWave();
+  makeWave();
+  noLoop();
 }
